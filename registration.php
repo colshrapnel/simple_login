c198561a7f1c2544ffa4844bbe61947f71d0e7cb
colshrapnel <colshrapnel@gmail.com>
2024-09-08 12:21:15 +0300

    Initial commit

---
+++ registration.php
@@ -0,0 +1,57 @@
+<?php
+// Code for the article https://phpdelusions.net/modern_php_example
+
+require 'init.php';
+
+// Initialize all values, so we won't have to always check them for existence
+$error = ['name' => '', 'email' => '', 'password' => ''];
+$input = ['name' => '', 'email' => ''];
+
+// if the form was submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    // input validation
+
+    $name = $input['name'] = trim(filter_input(INPUT_POST, 'name'));
+    if (strlen($name) < 3 || strlen($name) > 30) {
+        $error['name'] = 'Please enter your name, it must be from 3 to 30 charaters long.';
+    }
+
+    $email = $input['email'] = trim(filter_input(INPUT_POST, 'email'));
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $error['email'] = 'Please enter a valid email address.';
+    } else {
+        $result = $db->execute_query("SELECT 1 FROM users WHERE email = ?", [$email]);
+        if ($result->fetch_row()) {
+            $error['email'] = 'Email address already taken.';
+        }
+    }
+
+    $password = filter_input(INPUT_POST, 'password');
+    if (strlen($password) < 8 || strlen($password) > 72) {
+        $error['password'] = 'Please enter password, it must be from 8 to 72 characters long.';
+    }
+
+    // if no errors
+    if (implode($error) === '')
+    {
+        // passwords MUST be hashed using the dedicated function
+        $password = password_hash($password, PASSWORD_DEFAULT);
+        
+        // a parameterized query MUST be used to avoid errors and injections
+        $sql = "INSERT INTO users (name, email, password) VALUES (?,?,?)";
+        $db->execute_query($sql, [$name, $email, $password]);
+
+        // after each successful POST request there MUST be a redirect
+        header("Location: /index.php");
+        // after sending Location header the code MUST be terminated
+        die;
+    }
+}
+// Now, in case there was no POST request
+// Or it was but with errors
+// Start the output.
+// We have $error and $input arrays defined and filled above to be used in the templates
+require 'templates/layout/header.php';
+require 'templates/registration.php';
+require 'templates/layout/footer.php';
